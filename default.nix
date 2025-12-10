2fc25f00512dff3961f9779b3f7d24b692c80d16
cEEnthetic <belme.laszlo.janos@gmail.com>
2025-12-10 20:35:55 +0100

    goodbye unneccessary files!

--- default.nix
+++
@@ -1,7 +0,0 @@
-let 
-  nixpkgs = fetchTarball "https://github.com/NixOs/nixpkgs/tarball/nixos-25.05";
-  pkgs = import nixpkgs { config ={}; overlays = []; };
-in
-{
-  aretext = pkgs.callPackage ./aretext.nix {};
-}
